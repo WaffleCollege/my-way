d44b445e21630cedf738348b6c29ec39176cacf9
kawa3sayaka <115607345+kawa3sayaka@users.noreply.github.com>
2024-02-16 21:51:53 +0900

    react-router-dom 導入

--- my-way/src/App.js
+++ my-way/src/App.js
@@ -1,11 +1,12 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import './App.css';
 import Post from './components/post/Post';
 
 function App() {
   return (
-    <div className="app">
-      <Post/>
-    </div>
+      <div className="app">
+        <Post/>
+      </div>
   );
 }
 
